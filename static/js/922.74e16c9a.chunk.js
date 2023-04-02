7604234ebf616847757f15f008b3a2a1b2e24ba6
KaterinaPilipets <109768011+KaterinaPilipets@users.noreply.github.com>
2023-04-02 00:25:52 +0000

    Deploying to gh-pages from @ KaterinaPilipets/goit-react-hw-05-movies@8c09f26070ef95eed5cf99f45ce1df1ab57d53f8 🚀

---
+++ static/js/922.74e16c9a.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[922],{922:function(e,t,r){r.r(t);var n=r(184);t.default=function(){return(0,n.jsx)("b",{children:"Sorry, page not found!"})}}}]);
+//# sourceMappingURL=922.74e16c9a.chunk.js.map
